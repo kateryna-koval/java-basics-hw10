53c02beae6c49098971ac4a6f870ac3ca57ff192
Kateryna Koval <eqv23i@gmail.com>
2022-06-03 23:10:48 +0300

    HW 10

--- src/ExampleException.java
+++ src/ExampleException.java
@@ -1,8 +1,12 @@
 public class ExampleException {
-  public static int rectangleArea(int a, int b) {
-    // TODO put your code here
-    return 0;
-  }
+    public static int rectangleArea(int a, int b) {
+        int area;
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Input value is below zero.");
+        } else
+            area = a * b;
+        return area;
+    }
 }
 
 
--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -11,37 +11,45 @@ public class ExampleExceptionTest {
    *
    * @return test data array
    */
-  @DataProvider(name = "data")
-  public static Object[][] data() {
-    return new Object[][]{
-        {2, 2, 4},
-        {2, 3, 6}
-        // TODO add 2 more test data here
-    };
-  }
+    @DataProvider(name = "data")
+    public static Object[][] data() {
+        return new Object[][]{
+            {2, 2, 4},
+            {2, 3, 6},
+            {3, 3, 9},
+            {10, 2, 20}
+        };
+    }
 
   /**
    * Test data for exception test.
    *
    * @return test data array
    */
-  @DataProvider(name = "negativeData")
-  public static Object[][] negativeData() {
-    return new Object[][]{
-        {-2, 2},
-        {2, -2}
-        // TODO add 2 more test data here
-    };
-  }
+    @DataProvider(name = "negativeData")
+    public static Object[][] negativeData() {
+        return new Object[][]{
+            {-2, 2},
+            {2, -2},
+            {-3, 3},
+            {0, -21}
+         };
+    }
 
-  @Test(dataProvider = "data")
-  public void testRectangleArea(int a, int b, int c) {
-    // TODO put your code here
-  }
+    @Test(dataProvider = "data")
+    public void testRectangleArea(int a, int b, int c) {
+        int result = ExampleException.rectangleArea(a, b);
+        assertEquals(result, c, "Input value is correct.");
+    }
 
 
-  @Test(dataProvider = "negativeData")
-  public void testRectangleAreaNegative(int a, int b) {
-    // TODO put your code here
-  }
+    @Test(dataProvider = "negativeData")
+    public void testRectangleAreaNegative(int a, int b) {
+        try {
+            ExampleException.rectangleArea(a, b);
+            fail("Expected an Illegal Argument Exception to be thrown.");
+        } catch (IllegalArgumentException e) {
+            assertEquals(e.getMessage(), "Input value is below zero.");
+        }
+    }
 }
